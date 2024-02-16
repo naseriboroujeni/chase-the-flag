3e4b4f5dc59647c23e8f25d9f9e69d47faa7b463
naseriboroujeni <naseriboroujeni@gmail.com>
2024-02-16 12:18:25 +0330

    Fix Bug in onClose Method

--- server/source/GameServer.cpp
+++ server/source/GameServer.cpp
@@ -77,6 +77,7 @@ void GameServer::onClose(ConnectionHdl hdl) {
          leavingPlayer->getRoom()->removeUser(leavingPlayer);
          users.erase(leavingPlayer->getTag());
          delete leavingPlayer;
+         break;
       }
    }
 }
